560118afcf0f3ed9f09ae2123af53b64646e0871
Marco <empleomarcotapia@gmail.com>
2024-04-03 08:17:59 -0700

    Initial Commit

---
+++ Imagen4/Imagen4App.swift
@@ -0,0 +1,17 @@
+//
+//  Imagen4App.swift
+//  Imagen4
+//
+//  Created by Marco on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Imagen4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
